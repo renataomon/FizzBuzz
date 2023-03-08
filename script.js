1b383843d58e4f78022002434707ad2c595d052d
Renato M <60158844+renataomon@users.noreply.github.com>
2023-03-07 22:15:41 -0300

    Add "FizzBuzz" Exercise for The Odin Project

---
+++ script.js
@@ -0,0 +1,20 @@
+﻿function fizzBuzz() {
+    let inputValue = document.getElementById('textInput').value;
+    let outputValue = "";
+    for (let i = 1; i <= inputValue; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+            outputValue += "FizzBuzz" + "<br>";
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+            outputValue += "Fizz" + "<br>";
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+            outputValue += "Buzz" + "<br>";
+        } else {
+            console.log(i);
+            outputValue += i + "<br>";
+        }
+    }
+    document.getElementById('output').innerHTML = outputValue;
+  }
